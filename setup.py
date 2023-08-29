c50341a089f4066387da65433e09d5f760f06343
Bhaskar <bhaskar.uprety@fortunaidentity.com>
2023-08-29 22:00:08 +0530

    Including static files in pip

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='FastGen',
     version='0.0.1',
     packages=find_packages(),
+    package_data={'FastGen': ['FastGen/templates/*']},
     install_requires=[
         'Jinja2',
     ],
