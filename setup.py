ebee1936f5a46d3f08c80c4b015e33a06f0b86d8
Bhaskar <bhaskar.uprety@fortunaidentity.com>
2023-08-30 08:40:23 +0530

    Including all j2 files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     # package_data={'FastGen': ['templates/*']},
-    package_data={'': ['LICENSE'], 'FastGen': ['templates/*']},
+    package_data={'': ['LICENSE'], 'FastGen': ['*.j2']},
     install_requires=[
         'Jinja2',
     ],
